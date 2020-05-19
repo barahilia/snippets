deff7cad23925666d4bc1b33badbdfbb9ad0350d
Ilia Barahovsky <ilia.barahovsky@mobileye.com>
2020-05-19 16:10:17 +0300

    Setup directories layout

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt install tmux git vim
+
+rmdir \
+    ~/Desktop ~/Templates ~/Documents \
+    ~/Music ~/Pictures ~/Videos
+
+mkdir \
+    ~/bin ~/data ~/proj ~/repos ~/tmp \
+    ~/.local ~/.local/bin
