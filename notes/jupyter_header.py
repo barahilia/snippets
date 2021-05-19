c4e2759a7e37bfef9ee5268d34f0effc4feaed4d
Ilia Barahovsky <ilia.barahovsky@mobileye.com>
2021-05-19 08:33:29 +0300

    Handy jupyter notebook header

---
+++ notes/jupyter_header.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+%matplotlib notebook
+np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
+pd.options.display.float_format = '{:.4f}'.format
