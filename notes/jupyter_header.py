a7156f94e8307d4a2c062d75e32f824b2b45cbd4
Ilia Barahovsky <ilia.barahovsky@mobileye.com>
2022-02-01 07:49:38 +0200

    Use seaborn in plots

--- notes/jupyter_header.py
+++ notes/jupyter_header.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import cv2 as cv
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 %matplotlib notebook
 np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
 pd.options.display.float_format = '{:.4f}'.format
+sns.set_theme()
