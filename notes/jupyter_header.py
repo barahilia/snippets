b059cc2e1fc3025f62aa34683c1672d456dbdc3a
Ilia Barahovsky <ilia.barahovsky@mobileye.com>
2024-12-16 08:43:22 +0200

    Support interactive plots in new Jupyter

--- notes/jupyter_header.py
+++ notes/jupyter_header.py
@@ -4,7 +4,7 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-%matplotlib notebook
-np.set_printoptions(precision=4, suppress=True)
+%matplotlib ipympl
+np.set_printoptions(precision=4, suppress=True, threshold=40)
 pd.options.display.float_format = '{:.4f}'.format
 sns.set_theme()
